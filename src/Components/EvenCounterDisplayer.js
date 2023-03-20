bdd27f07baecabeef7aa698752009c2f3243c15b
Scwtb8235 <124462310+Scwtb8235@users.noreply.github.com>
2023-03-20 15:16:14 -0400

    second commit

--- src/Components/EvenCounterDisplayer.js
+++ src/Components/EvenCounterDisplayer.js
@@ -3,14 +3,22 @@ class EvenCounterDisplayer extends Component {
 
 render() {
 const evenCounter = Math.floor(this.props.EvenCounter / 2) * 2;
-
-
+// let x = [];
+// const evenCounter=()=>{
+//     for (; x <= x.length; x=+2) {
+//     if (x%2===0) {
+//         console.log(x + " is even");
+//     } 
+//     }
+//     }
     return (
     <>
         <h3>Even number Displayer :{evenCounter}</h3>
     </>
     )
+
 }
 }
 
 export default EvenCounterDisplayer
+
--- src/Components/IncreaseDecreaseCount.js
+++ src/Components/IncreaseDecreaseCount.js
@@ -3,7 +3,7 @@ function IncreaseDecreaseCount() {
     const [count,setCount] = useState(0);
 return (
     <div>
-        <div> "Value Holder" Inital Count: {count}</div>
+        <h1> Inital Count: {count}</h1>
         <button onClick={()=>setCount(0)}>Reset</button>
         <button onClick={()=>setCount(count+1)}>Increase</button>
         <button onClick={()=>setCount(count-1)}>Decrease</button>
